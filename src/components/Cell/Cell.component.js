7d0932044f98105f6f16e33188c358c2711d1fc3
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-07-23 16:37:33 +0530

    cell can tell onclick if blank cell is adjacent

--- src/components/Cell/Cell.component.js
+++ src/components/Cell/Cell.component.js
@@ -2,18 +2,41 @@ import React, {Component} from 'react';
 import './Cell.css'
 
 class Cell extends Component {
+    constructor(props){
+        super(props);
+        this.handleCellClick = this.handleCellClick.bind(this);
+        this.state={
+            value: -1,
+        };
+    }
+
+    // emptyCellAdjacent(emptyCellX,emptyCellY){
+
+    // }
+
+    handleCellClick(){
+        console.log(this.props.adjacent);
+        // if(emptyCellAdjacent){
+
+        // }
+    }
+
+    componentDidMount(){
+        this.setState({
+            value: this.props.number
+        })
+    }
 
     displayCell(number){
         if(number === -1){
             return(
                 <div className="grid-item black">
-                    <span>{number}</span>
+                    {number}
                 </div>
             )
         }else {
         return(
-
-            <div className="grid-item">
+            <div className="grid-item" onClick={this.handleCellClick}>
                 <span>{number}</span>
             </div>
         )}
@@ -21,8 +44,7 @@ class Cell extends Component {
 
     render(){
         const {number} = this.props;
-        return this.displayCell(number)
-
+        return this.displayCell(number);
     }
 }
 
--- src/components/GameGrid/GameGrid.component.js
+++ src/components/GameGrid/GameGrid.component.js
@@ -12,16 +12,23 @@ class GameGrid extends Component {
         }
     };
 
+    checkAdjacency(i,j){
+        const {emptyCellX, emptyCellY} = this.state;
+        if((emptyCellX === i+1 && emptyCellY===j) 
+            || (emptyCellX === i-1 && emptyCellY===j)
+            || (emptyCellY === j+1 && emptyCellX===i)
+            || (emptyCellY === j-1 && emptyCellX===i)) return true;
+        else return false;
+    }
+
     renderCells(cells){
         const gridSize = cells.length;
         let cellContainer=[];
-        // console.log(cells,gridSize);
         for(var i=0;i<gridSize;i++){
             const cellRow = cells[i];
             console.log(cellRow);
             for(var j=0;j<cellRow.length;j++){
-                console.log(cellRow[j]);
-                cellContainer.push(<Cell number={cellRow[j]} />); 
+                cellContainer.push(<Cell number={cellRow[j]} adjacent={this.checkAdjacency(i,j)}/>); 
             }
         }
         return cellContainer;
