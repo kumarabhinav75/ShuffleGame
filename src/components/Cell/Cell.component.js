0fe942a216d68fdfcf059d09dc7c189df623e4ad
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-07-23 15:49:46 +0530

    added Cell and GameGrid components

---
+++ src/components/Cell/Cell.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Cell extends Component {
+    constructor(props){
+        super(props);
+    };
+
+    render(){
+        return();
+    }
+}
+
+export default Cell;
